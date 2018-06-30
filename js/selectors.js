b3a072caf78ecac091ce7916cf2b0628d6ade3a3
Stephanie Guzman <guzman.stephaniec@gmail.com>
2018-06-30 01:44:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+ return $('p')
+}
+
+function lastImageSelector() {
+ $('div:last')
+}
+
+function ninjaBabySelector() {
+ $('#baby-ninja') 
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('ul li')
+  $('parent-tag child-tag:first-child')
+  $('div img: first-child')
+}
