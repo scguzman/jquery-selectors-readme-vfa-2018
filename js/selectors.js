ea4b13a3f254a4e78b80638eaf3b970e495fb905
Stephanie Guzman <guzman.stephaniec@gmail.com>
2018-06-30 01:39:50 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,4 +3,18 @@ function paragraphSelector() {
  return $('p')
 }
 
+function lastImageSelector() {
+  return 
+}
 
+function ninjaBabySelector() {
+  id selector ninja baby image
+}
+
+function divSelector() {
+  div pics
+}
+
+function firstListItem() {
+  first-child selector to return first list item in the ul with the ID pic-list
+}
